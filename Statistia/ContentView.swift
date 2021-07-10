ec64eda04588827dd7596ce39d3cebddf0071875
Prajjwal009 <sinhaprajjwak009@gmail.com>
2021-07-10 20:37:13 +0530

    Initial Commit

---
+++ Statistia/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Statistia
+//
+//  Created by Prajjwal on 10/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
