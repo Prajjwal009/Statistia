461b0819167512e0067095efe25b2f1ea38590f8
Prajjwal009 <sinhaprajjwak009@gmail.com>
2021-07-12 21:27:33 +0530

    Task Setter 2nd dev

--- Statistia/TaskSetter.swift
+++ Statistia/TaskSetter.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct TaskSetter: View {
+    @Environment(\.presentationMode) var presentationMode
     @State var taskTime : Double = 0.0
     @State var taskText : String = ""
     @State var taskInfoText : String = ""
+    @State var taskDate  = Date()
+    @State var color1 = Color("Color1")
     var body: some View {
         VStack(){
             HStack(alignment: .bottom){
-                TextField("Task",text: $taskText)
+                TextField("task",text: $taskText)
                     .padding(.horizontal)
                     .padding(.top,20)
                     .frame(height : 80)
@@ -31,7 +34,9 @@ struct TaskSetter: View {
             Rectangle()
                 .frame(width : UIScreen.main.bounds.width - 10 ,height: 2)
             
-            TextField("About it",text : $taskInfoText)
+            TextField("about it",text : $taskInfoText)
+                .font(.system(size: 20))
+                
                 .padding(.horizontal)
                 
                 .frame(height : 60)
@@ -41,15 +46,29 @@ struct TaskSetter: View {
                 .padding(.top)
                 .padding(.horizontal,10)
             
+            HStack {
+                Text("Time Duration")
+                    .font(.system(size: 22))
+                    .fontWeight(.semibold)
+                    .padding()
+                Spacer()
+            }
+            
+            
             ZStack{
                 
                 Circle().stroke(Color.purple,lineWidth: 2)
                     .frame(width : 150, height :150)
-                    .foregroundColor(.white)
+                    .foregroundColor(color1)
                     
-                Text("\(Int(taskTime))")
-                    .font(.system(size: 28))
-                    .fontWeight(.light)
+                VStack(alignment : .center){
+                    Text("\(Int(taskTime))")
+                        .font(.system(size: 30))
+                        .fontWeight(.bold)
+                    Text("min")
+                        .fontWeight(.light)
+                }
+                .padding(.top,18)
                 
             }
             .padding(.top,30)
@@ -59,11 +78,38 @@ struct TaskSetter: View {
             
             Slider(value: $taskTime, in : 0...500)
                 .padding()
-                .accentColor(.black.opacity(0.8))
+                .accentColor(color1)
+            HStack{
+                Text("Date")
+                    .font(.system(size: 22))
+                    .fontWeight(.semibold)
+                
+                DatePicker("",selection : $taskDate,in : Date()...)
+                    .accentColor(color1)
+            }
+            .padding()
+            .padding(.vertical,10)
+            
+            
+                
             
            
               
             Spacer()
+            Button(action : {
+                presentationMode.wrappedValue.dismiss()
+                
+                
+            }){
+                Text("Done")
+                    .foregroundColor(color1)
+                    .font(.system(size: 20))
+                    
+                    .frame(width : 200,height: 50)
+                    
+                    .background(Color.gray.opacity(0.2))
+                    .clipShape(Capsule())
+            }
                 
               
         }
